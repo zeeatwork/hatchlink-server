73bf6800a247368db6411f2b53bfe9912eb5a522
Zenzi Ali <zeeathome935@gmail.com>
2020-09-01 05:58:47 -0400

    first commit

---
+++ src/app.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const helmet = require("helmet");
+const { NODE_ENV } = require("./config");
+const validateBearerToken = require("./validate-bearer-token");
+const errorHandler = require("./error-handler");
+const foldersRouter = require("./Resources/resources-router");
+const notesRouter = require("./Reviews/reviews-router");
+
+const app = express();
+
+const morganOption = NODE_ENV === "production" ? "tiny" : "common";
+
+app.use(morgan(morganOption));
+app.use(helmet());
+app.use(cors());
+//app.use(validateBearerToken);
+
+app.get("/", (req, res) => {
+  res.send("Hello, world!");
+});
+
+app.use(errorHandler);
+app.use("/api/reviews", reviewsRouter);
+app.use("/api/resources", resourcesRouter);
+
+module.exports = app;
